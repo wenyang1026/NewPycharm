47a8213b573a9ecc6a6c55b00031e235a0a08c5a
wenyang1026 <wy27@rice.edu>
2023-09-01 16:12:11 -0500

    The program outputs 'Hello, world!'

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
